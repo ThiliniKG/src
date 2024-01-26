e51d7f47e3c855b75437593a00bd3aea10c9ac20
Thilini Gamage <thilinifin@outlook.comexample.com>
2024-01-26 11:51:13 +0200

    Create ex1.php

---
+++ ex1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1: Getting Started with PHP - Thilini</title>
+</head>
+<body>
+    <h3>Write PHP code to output the following message:
+    Hello world! My name is "David"
+    </h3>
+    <?php
+    //Use backslash to escape special characters like"
+    echo"Hello world! My name is \"David"\";
+    ?>
+
+
+
+
+
+
+<table>
+  <tr><th>
+
+
+
+    
+</body>
+</html>
