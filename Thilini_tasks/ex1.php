c0d9d4bf607442a4f9f25f1fe363b1b3343085ef
Thilini Gamage <thilinifin@outlook.comexample.com>
2024-01-28 22:13:42 +0200

    Update ex1.php

--- Thilini_tasks/ex1.php
+++ Thilini_tasks/ex1.php
@@ -51,7 +51,7 @@
         </tr>
     </table>    
 
-  <h3>Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.</h3> 
+  <h3> 3.4 Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.</h3> 
   <img src="images/image24.png" alt="Web development">
 </body>
 </html>
