c1f87df29ff36f2c8040cb9c2767dd79f3d685b5
Thilini Gamage <thilinifin@outlook.comexample.com>
2024-01-28 21:40:38 +0200

    Update ex1.php

--- Thilini_tasks/ex1.php
+++ Thilini_tasks/ex1.php
@@ -6,7 +6,7 @@
     <title>Exercise 1: Getting Started with PHP - Thilini</title>
 </head>
 <body>
-    <h3>Write PHP code to output the following message:
+    <h3>3.1 Write PHP code to output the following message:
     Hello world! My name is "David"
     </h3>
     <?php
