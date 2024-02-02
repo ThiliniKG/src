c5b405f53011bf74dfe1d0e6a99a3c78677852a7
Thilini Gamage <thilinifin@outlook.comexample.com>
2024-02-02 22:36:20 +0200

    FORM

--- Thilini_tasks/ex3.php
+++ Thilini_tasks/ex3.php
@@ -5,14 +5,54 @@ include "header.php" ?>
 <h1> 3.2 Create a simple HTML form to collect the user’s Firstname and Lastname. Use the echo statement to 
     print “Hello [Firstname] [Lastname], You are welcome to my site".inside an h3.”</h1>
 
-<form method="post" action="process.php">
-   Name:<input type="text" name="nimi"required> <br> <br>
-   Email:<input type="email" name="email"required> <br> <br>
-   DOB:<input type="date" name="dob"> <br> <br>
-   Select Color:<input type="color" name="color"> <br> <br>
-   <input type="Submit" value="Submit">
+<form method="post" action= <?php htmlspecialchars($_SERVER["PHP_SELF"]);?>>
 
-</form>    
+FirstName:<input type="text"  name="firstName" placeholder="Enter your first name" required><br><br>
+
+Lastname:<input type="text"  name="lastName" placeholder="Enter your last name" required><br><br>      
+
+<button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
+
+
+
+<?php
+
+// PHP code to handle form submission and display the greeting
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $firstName = $_POST["firstName"];
+
+    $lastName = $_POST["lastName"];
+
+    echo "<h3>Hello $firstName $lastName, You are welcome to my site.</h3>";
+
+}
+
+?>
+
+
+
+<br>
+
+<br>
+
+<br> 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Thilini_tasks/process.php
+++ Thilini_tasks/process.php
@@ -1,13 +1,14 @@
 <?php
+//what to do with the form data
 $title="Your information";
 include "header.php" ?>
 
-//what to do with the form data
 
-$nimi =$_post ["nimi"];
-$email =$_post ["email"];
 
-echo "Hello" . $nimi ."<br>Your email id is: ".$email;
+$nimi =$_POST ["nimi"];
+$email =$_POST ["email"];
+
+echo "Hello" . $nimi .<br>"Your email id is: ".$email;
 
 
 <?php include "footer.php" ?>
