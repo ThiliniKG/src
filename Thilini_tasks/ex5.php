0d9a247d8e4882e87fd273b9d3ca8e879f546e7f
Thilini Gamage <thilinifin@outlook.comexample.com>
2024-02-08 10:22:57 +0200

    ex5done

--- Thilini_tasks/ex5.php
+++ Thilini_tasks/ex5.php
@@ -5,18 +5,30 @@ include "header.php" ?>
 <h2>About us</h2>
 
 <script>
+    //this is a comment
  document.write   ("Hello world")
+
+ // triggering an alert box
+ windows.alert("This is an alert box")
+
 </script>
 
 <noscript>
 if javascript is not enabled this will be displayed so users know they need to enable js.
 </noscript>
 
+<script>
+    //document.writw but after loading the page
+
+    function hello(){ 
+
+    document.write("This is after the page has loaded")
+
+    }
+</script>
 
 
-
-
-
+<button onclick="document.write('This is something')">click me</button>
 
 
 
